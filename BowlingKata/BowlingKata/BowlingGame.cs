703e73aff0176a49fae33c5438e695d5852f1b2f
Raúl Corchero <corchero100@gmail.com>
2024-09-27 12:46:24 +0100

    refactor extract methods

--- BowlingKata/BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingKata/BowlingGame.cs
@@ -9,17 +9,26 @@ public class BowlingGame
 		var totalScore = 0;
 		foreach (var score in scoreForRoll)
 		{
-			var firstAttempt = score[0].ToString();
-			if (firstAttempt != "-")
-			{
-				totalScore += Convert.ToInt32(score[0].ToString());
-			}
-			var secondAttempt = score[1].ToString();
-			if (secondAttempt != "-") {
-				totalScore += Convert.ToInt32(score[1].ToString());
-			}
+			totalScore += GetScoreForRoll(score);
 		}
 
 		return totalScore;
+	}
+
+	private int GetScoreForRoll(string roll)
+	{
+		var score = 0;
+		score += GetScoreForAttempt(roll[0]);
+		score += GetScoreForAttempt(roll[1]);
+		return score;
+	}
+
+	private int GetScoreForAttempt(char attempt)
+	{
+		if (attempt == '-') {
+			return 0;
+		}
+
+		return Convert.ToInt32(attempt.ToString());
 	}
 }
